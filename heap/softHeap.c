4388441795a58e10ef3dc2470ba6f1c2a07bd764
Xieyang <ly1126225520@gmail.com>
2019-08-17 23:26:16 +0800

    heap代码编辑完成

--- heap/softHeap.c
+++ heap/softHeap.c
@@ -1,57 +1,91 @@
 #include "../softType.h"
 #include "./softHeap.h"
+#include "stdlib.h"
 
     
-/*
-*初始化softHeap
-*参数:  hSoftHeap:要操作的栈，heapBuf栈缓存，itemSize栈元素大小
-*返回值:
-*/    
+ 
 int32_t softHeapinitialize(struct softHeapCtrlType *hSoftHeap,void *heapBuf,size_t bufSize,size_t itemSize)
 {
     if(heapBuf == NULL) return softHeapFail;
     hSoftHeap->heap = heapBuf;
     hSoftHeap->itemSzie = itemSize;
-    hSoftHeap->heapDeepth = bufSize;
+    hSoftHeap->heapCapaciity = bufSize;
     hSoftHeap->heapPtr = 0;
     return softHeapOK;
 }
 
-
-
-
 int32_t softHeapPush(struct softHeapCtrlType *hSoftHeap,void *item)
 {
+    char *des = (uint32_t)hSoftHeap->heap + hSoftHeap->heapPtr *hSoftHeap->itemSzie;
+    char *src = item;
+    if(hSoftHeap == NULL) return softHeapPtrNull;
+    if(item == NULL) return softHeapParamErr;
+    if(hSoftHeap->heapPtr >= hSoftHeap->heapCapaciity ) return softHeapFull;
 
+    //数据转移
+    for (size_t i = 0 ; i < hSoftHeap->itemSzie; i++)
+    {
+        *(des++) = *(src++);
+    }
 
+    //指针移动
+    hSoftHeap->heapPtr++;
 
+    return softHeapOK;
 }
-
 
 int32_t softHeapPop(struct  softHeapCtrlType *hSoftHeap,void *item)
 {
+    char *src = (uint32_t)hSoftHeap->heap + hSoftHeap->heapPtr *hSoftHeap->itemSzie;
+    char *des = item;
+    if(hSoftHeap == NULL) return softHeapPtrNull;
+    if(item == NULL) return softHeapParamErr;
+    if(hSoftHeap->heapPtr <= 0 ) return softHeapEmpty;
+
+    //数据转移
+    for (size_t i = 0 ; i < hSoftHeap->itemSzie; i++)
+    {
+        *(des++) = *(src++);
+    }
+
+    //指针移动
+    hSoftHeap->heapPtr--;
     
-};
-
-
-int32_t softHeapPopAll(struct softHeapCtrlType *hSoftHeap)
-{
-
-
+    return softHeapOK;
 }
 
+int32_t softHeapReset(struct softHeapCtrlType *hSoftHeap)
+{
+    if(hSoftHeap == NULL) return softHeapPtrNull;
 
+    //清除数据
+    memset(hSoftHeap->heap,0,hSoftHeap->heapCapaciity * hSoftHeap->itemSzie);
 
+    //更新指针
+    hSoftHeap->heapPtr = 0;
 
+    return softHeapOK;
+}
 
+int32_t softHeapGetItemQuantity(struct softHeapCtrlType *hSoftHeap)
+{
+    if(hSoftHeap == NULL) return softHeapPtrNull;
 
+    return hSoftHeap->heapPtr;
+}
 
+int32_t softHeapGetCapaciity(struct softHeapCtrlType *hSoftHeap)
+{
+    if(hSoftHeap == NULL) return softHeapPtrNull;
 
+    return hSoftHeap->heapCapaciity;
+}
 
+int32_t softHeapGetFreeCapaciity(struct softHeapCtrlType *hSoftHeap)
+{
+    if(hSoftHeap == NULL) return softHeapPtrNull;
+    if(hSoftHeap->heapPtr > hSoftHeap->heapCapaciity) return softHeapOverFlow;
 
-
-
-
-
-
+    return hSoftHeap->heapCapaciity - hSoftHeap->heapPtr;
+}
 
--- heap/softHeap.h
+++ heap/softHeap.h
@@ -7,38 +7,85 @@
 #define _SOFT_HEAP_H__
 #include "../softType.h"
 
-#define softHeapOK      0
-#define softHeapFail    1
+#define softHeapOK          0
+#define softHeapFail        -1
+#define softHeapFull        -2
+#define softHeapEmpty       -3
+#define softHeapOverFlow    -4
+#define softHeapParamErr    -5
+#define softHeapNotFound    -6
+#define softHeapPtrNull     -7
 
 
-
+#define softHeapCtrlTypeDef(name,heap_cap,i_size)   uint8_t name##HeapBuf[heap_cap*i_size];  \
+struct softHeapCtrlType name= \
+{   \
+    .heap = name##HeapBuf,\
+    .heapCapaciity = heap_cap,\
+    .itemSzie = i_size,\
+    .heapPtr = 0,\
+}
 
 struct softHeapCtrlType
 {
     void *heap;
-    uint32_t heapDeepth;
-    uint32_t heapPtr;
+    int32_t heapCapaciity;
+    int32_t heapPtr;
     size_t itemSzie;
 };
 
 
+/*
+*   基础操作
+*/
+
+/*
+*初始化softHeap,用于自己创建
+*参数:  hSoftHeap:要操作的栈指针，heapBuf栈缓存，itemSize栈元素大小
+*返回值:操作成功softHeapOK，否作，返回故障信息
+*/   
 int32_t softHeapinitialize(struct softHeapCtrlType *hSoftHeap,void *heapBuf,size_t bufSize,size_t itemSize);
 
+/*
+*将数据入栈
+*参数：hSoftHeap:要操作的栈指针，item要压入的元素
+*返回值:操作成功softHeapOK，否则返回故障信息
+*/
+int32_t softHeapPush(struct softHeapCtrlType *hSoftHeap,void *item);
 
+/*
+*弹出栈顶数据
+*参数：hSoftHeap:要操作的栈指针，item接收数据的buf
+*返回值:操作成功softHeapOK，否则返回故障信息
+*/
+int32_t softHeapPop(struct  softHeapCtrlType *hSoftHeap,void *item);
 
+/*
+*栈数据复位，清空所有数据
+*参数：hSoftHeap:要操作的栈指针
+*返回值:操作成功softHeapOK，否则返回故障信息
+*/
+int32_t softHeapReset(struct softHeapCtrlType *hSoftHeap);
 
+/*
+*获取栈内数据量
+*参数：hSoftHeap:要操作的栈指针
+*返回值:元素数量，如果hSoftHeap为空，则返回错误
+*/
+int32_t softHeapGetItemQuantity(struct softHeapCtrlType *hSoftHeap);
 
+/*
+*获取栈总容量
+*参数：hSoftHeap:要操作的栈指针
+*返回值:栈总容量，如果hSoftHeap为空，则返回错误
+*/
+int32_t softHeapGetCapaciity(struct softHeapCtrlType *hSoftHeap);
 
-
-
-
-
-
-
-
-
-
-
-
+/*
+*获取剩余容量
+*参数：hSoftHeap:要操作的栈指针
+*返回值:栈剩余容量，如果hSoftHeap为空，则返回错误
+*/
+int32_t softHeapGetFreeCapaciity(struct softHeapCtrlType *hSoftHeap);
 
 #endif
